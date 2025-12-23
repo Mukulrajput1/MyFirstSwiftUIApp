aacd8455c96a9fff2957735c7ff6515fc954a445
Mukul Rajput <mukul2002rajput@gmail.com>
2025-12-23 17:14:13 +0530

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Purav Kapoor on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
